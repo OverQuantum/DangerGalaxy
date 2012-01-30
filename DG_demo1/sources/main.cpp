3aced71272e2d2c8175073644c599fb9d22c710f
OverQuantum <overqu@gmail.com>
2012-01-31 00:06:43 +0400

    Distorted relation btw RealSpace and HyperSpace 120130: ADD: distorted relation btw RealSpace and HyperSpace 120130: ADD: distorted relation of HyperSpace is mixed with encryption-like sbox

--- DG_demo1/sources/main.cpp
+++ DG_demo1/sources/main.cpp
@@ -270,6 +270,8 @@ coding demo1:
 120128: ADD: forcefield on FTL (by decal only)
 120128: ADD: forcefield in QuasiSpace (by decal only)
 120128: FIX: object left in interstellar space are now moves correctly on change quadrant (+autopilot breaked)
+120130: ADD: distorted relation btw RealSpace and HyperSpace
+120130: ADD: distorted relation of HyperSpace is mixed with encryption-like sbox
 
 
 DONE:
@@ -337,6 +339,7 @@ DONE:
 + HyperSpace : recalc stars on moving thru Galaxy
 + HyperSpace : timer-autopilot
 + FIX BUG: projectiles left in interstellar space are flying strange
++ HyperSpace : distorted relation to RealSpace
 
 
 TODO:
@@ -349,7 +352,7 @@ TODO:
   - "fog" (to limit visible stars not by rectangle screen)
   - approx map(?)
   - entering and exiting by hypergate/jumpsphere/etc
-  - distorted relation to RealSpace (? how distorted?)
+  - randomize distortion with RealSpace in interstellar
   - twirl depends on coordinates only (combined with distorted relation?)
   - nice graphic for background (2D flow algorithm ?)
   - in-HyperSpace quick-living objects (slowdown or speedup player; pull/push by gravity-like force e.t.c.)
@@ -1430,7 +1433,7 @@ public:
 	vector2ds GetGalaxyQuadrant() {return galaxyQuadrant;}
 	f64 GetControlThrust() {return controlThrust;}
 
-	void SetGalaxyCoordinates(vector2d newCoords) {galaxyCoordinates=newCoords;}
+	void SetGalaxyCoordinates(vector2d newCoords);
 
 	void UpdateGalaxyCoordinates(u32 type);
 
@@ -1480,6 +1483,9 @@ public:
 	u64 text2StargateAddress(text_string text);//convert text to address
 	text_string StargateAddress2text(u64 address);//convert text to address
 
+	vector3d Galaxy2HyperSpace(vector2d galaxyCoordinates);//convert galaxy coordinates to HyperSpace coordinates
+	vector2d HyperSpace2Galaxy(vector3d hyperSpaceCoordinates);//convert HyperSpace coordinates to galaxy coordinates
+
 	//void DebugDump();//ONLY for debug - dump count of stars in regions 64x64 ly
 	void SaveMapTexture();
 
@@ -1490,6 +1496,10 @@ public:
 	void DebugShowWormholes();
 
 private:
+
+	static const u8 sBox[256];
+	static const u8 sBoxInv[256];
+
 	bool wormholesGenerated;
 	u32 debugStarCounter;
 	u32 galaxyType;//0 - E0, 1 - E1-E7, 2 - S*, 3 - Irr random, 4 - E* two cores
@@ -6504,10 +6514,12 @@ void HyperSpaceView::AddStar(GalaxyStarSystem* starSystem)
 	vector2d starGalaxyPos;
 	SpaceObject* starMarker = resourceManager->MakeGravityWell(0.001*LIGHTYEAR/HYPERSPACE_SCALE);
 
+	/*
 	starGalaxyPos = starSystem->GetGalaxyCoordinates();
 	starHyperPos.X = starGalaxyPos.X*LIGHTYEAR/HYPERSPACE_SCALE;
 	starHyperPos.Y = starGalaxyPos.Y*LIGHTYEAR/HYPERSPACE_SCALE;
-	starHyperPos.Z = 0;
+	starHyperPos.Z = 0;*/
+	starHyperPos = gameRoot->GetGalaxy()->Galaxy2HyperSpace(starSystem->GetGalaxyCoordinates());
 
 	starMarker->SetIntProperty(SpaceObject::EMIT_GRAVITY,0);
 	starMarker->SetIntProperty(SpaceObject::AFFECTED_BY_GRAVITY,0);
@@ -6530,7 +6542,7 @@ void HyperSpaceView::RemoveAllStars()
 	}
 }
 
-//flags: 1 - return galaxy coordinates (default: physics)
+//flags: none
 vector2d HyperSpaceView::GetCoordinatesOnScreen(vector2ds pixelCoords, u32 flags)
 {
 	core::dimension2d<u32> screenSize = videoDriver->getScreenSize();
@@ -6541,12 +6553,13 @@ vector2d HyperSpaceView::GetCoordinatesOnScreen(vector2ds pixelCoords, u32 flags
 	gameCoordinates.X=playerShip->GetPosition().X+kfov*((gameCoordinates.X-screenSize.Width*.5)/screenSize.Height);
 	gameCoordinates.Y=playerShip->GetPosition().Y-kfov*(gameCoordinates.Y/screenSize.Height-0.5);
 
+	/*
 	if ((flags&1)!=0)
 	{
 		//galaxy coordinates needed
 		gameCoordinates*=HYPERSPACE_SCALE/LIGHTYEAR;
 		
-	}
+	}*/
 
 	return gameCoordinates;
 }
@@ -7033,7 +7046,10 @@ void DG_Game::Update() {
 	case SpaceObject::MOTION_HYPER:
 		playerShip->UpdateGalaxyCoordinates(1);//in hyper
 		wholeGalaxy->UpdatePlayerLocation(playerShip->GetGalaxyCoordinates());
-		wholeGalaxy->AddHyperSpaceObjects(HyperSpace,1);
+		wholeGalaxy->AddHyperSpaceObjects(HyperSpace,1);//will recreate star markers only if player location has changed
+
+		//known stars is updated by 50x50 ly, this may leads to "appearing" stars at 6 l.y. from player
+		//(if x = 80*n + 70 and decreased, star located at x=80*n+49..50 could appear at x=80*n+64
 
 		break;
 	default:
@@ -7269,9 +7285,10 @@ void DG_Game::GoStarSystem(GalaxyStarSystem* toStarSystem)
 
 void DG_Game::GoHyperSpace()
 {
-	f64 koef = LIGHTYEAR/HYPERSPACE_SCALE;
-	vector2d playerGalaxyCoordinates = playerShip->GetGalaxyCoordinates();
-	vector3d playerHyperSpacePos = vector3d(playerGalaxyCoordinates.X*koef,playerGalaxyCoordinates.Y*koef,0);
+	//f64 koef = LIGHTYEAR/HYPERSPACE_SCALE;
+	//vector2d playerGalaxyCoordinates = playerShip->GetGalaxyCoordinates();
+	//vector3d playerHyperSpacePos = vector3d(playerGalaxyCoordinates.X*koef,playerGalaxyCoordinates.Y*koef,0);
+	vector3d playerHyperSpacePos = wholeGalaxy->Galaxy2HyperSpace(playerShip->GetGalaxyCoordinates());
 
 	playerShip->SetIntProperty(SpaceObject::MOTION_TYPE,SpaceObject::MOTION_HYPER);
 	gamePhysics->GoHyperSpace();
@@ -7294,7 +7311,8 @@ void DG_Game::BreakHyperSpace()
 	//vector2d playerGalaxyCoordinates = vector2d(playerHyperSpacePos.X*koef,playerHyperSpacePos.Y*koef);
 
 	//playerShip->SetGalaxyCoordinates(playerGalaxyCoordinates);
-	playerShip->UpdateGalaxyCoordinates(1);//in hyper
+	//playerShip->UpdateGalaxyCoordinates(1);//in hyper
+	playerShip->SetGalaxyCoordinates(wholeGalaxy->HyperSpace2Galaxy(playerShip->GetPosition()));
 	playerShip->SetSpeed(vector3d(0));
 	playerShip->SetIntProperty(SpaceObject::MOTION_TYPE,SpaceObject::MOTION_NAVIGATION);
 
@@ -7527,14 +7545,24 @@ void PlayerShip::UpdateGalaxyCoordinates(u32 type)
 	case 1://HyperSpace
 		galaxyCoordinates.X = position.X * HYPERSPACE_SCALE/LIGHTYEAR;
 		galaxyCoordinates.Y = position.Y * HYPERSPACE_SCALE/LIGHTYEAR;
+		
+		//galaxyCoordinates = physics->GetRoot()->GetGalaxy()->
+
 		galaxyQuadrant.X = (s32)floor(galaxyCoordinates.X);
 		galaxyQuadrant.Y = (s32)floor(galaxyCoordinates.Y);
 		break;
 	default:
 		break;
 	}
-
 }
+
+void PlayerShip::SetGalaxyCoordinates(vector2d newCoords)
+{
+	galaxyCoordinates=newCoords;
+	galaxyQuadrant.X = (s32)floor(galaxyCoordinates.X);
+	galaxyQuadrant.Y = (s32)floor(galaxyCoordinates.Y);
+}
+
 
 void PlayerShip::JumpToQuasiSpace(s32 direction)
 {
@@ -7865,6 +7893,48 @@ void PlayerShip::SetControlThrust(f64 newThrust)
 
 //#########################################################################
 //Galaxy
+
+//Rijndael Forward S-box
+const u8 Galaxy::sBox[256] = {
+   0x63, 0x7C, 0x77, 0x7B, 0xF2, 0x6B, 0x6F, 0xC5, 0x30, 0x01, 0x67, 0x2B, 0xFE, 0xD7, 0xAB, 0x76,
+   0xCA, 0x82, 0xC9, 0x7D, 0xFA, 0x59, 0x47, 0xF0, 0xAD, 0xD4, 0xA2, 0xAF, 0x9C, 0xA4, 0x72, 0xC0,
+   0xB7, 0xFD, 0x93, 0x26, 0x36, 0x3F, 0xF7, 0xCC, 0x34, 0xA5, 0xE5, 0xF1, 0x71, 0xD8, 0x31, 0x15,
+   0x04, 0xC7, 0x23, 0xC3, 0x18, 0x96, 0x05, 0x9A, 0x07, 0x12, 0x80, 0xE2, 0xEB, 0x27, 0xB2, 0x75,
+   0x09, 0x83, 0x2C, 0x1A, 0x1B, 0x6E, 0x5A, 0xA0, 0x52, 0x3B, 0xD6, 0xB3, 0x29, 0xE3, 0x2F, 0x84,
+   0x53, 0xD1, 0x00, 0xED, 0x20, 0xFC, 0xB1, 0x5B, 0x6A, 0xCB, 0xBE, 0x39, 0x4A, 0x4C, 0x58, 0xCF,
+   0xD0, 0xEF, 0xAA, 0xFB, 0x43, 0x4D, 0x33, 0x85, 0x45, 0xF9, 0x02, 0x7F, 0x50, 0x3C, 0x9F, 0xA8,
+   0x51, 0xA3, 0x40, 0x8F, 0x92, 0x9D, 0x38, 0xF5, 0xBC, 0xB6, 0xDA, 0x21, 0x10, 0xFF, 0xF3, 0xD2,
+   0xCD, 0x0C, 0x13, 0xEC, 0x5F, 0x97, 0x44, 0x17, 0xC4, 0xA7, 0x7E, 0x3D, 0x64, 0x5D, 0x19, 0x73,
+   0x60, 0x81, 0x4F, 0xDC, 0x22, 0x2A, 0x90, 0x88, 0x46, 0xEE, 0xB8, 0x14, 0xDE, 0x5E, 0x0B, 0xDB,
+   0xE0, 0x32, 0x3A, 0x0A, 0x49, 0x06, 0x24, 0x5C, 0xC2, 0xD3, 0xAC, 0x62, 0x91, 0x95, 0xE4, 0x79,
+   0xE7, 0xC8, 0x37, 0x6D, 0x8D, 0xD5, 0x4E, 0xA9, 0x6C, 0x56, 0xF4, 0xEA, 0x65, 0x7A, 0xAE, 0x08,
+   0xBA, 0x78, 0x25, 0x2E, 0x1C, 0xA6, 0xB4, 0xC6, 0xE8, 0xDD, 0x74, 0x1F, 0x4B, 0xBD, 0x8B, 0x8A,
+   0x70, 0x3E, 0xB5, 0x66, 0x48, 0x03, 0xF6, 0x0E, 0x61, 0x35, 0x57, 0xB9, 0x86, 0xC1, 0x1D, 0x9E,
+   0xE1, 0xF8, 0x98, 0x11, 0x69, 0xD9, 0x8E, 0x94, 0x9B, 0x1E, 0x87, 0xE9, 0xCE, 0x55, 0x28, 0xDF,
+   0x8C, 0xA1, 0x89, 0x0D, 0xBF, 0xE6, 0x42, 0x68, 0x41, 0x99, 0x2D, 0x0F, 0xB0, 0x54, 0xBB, 0x16
+};
+
+//Rijndael Inverse S-box
+const u8 Galaxy::sBoxInv[256] = 
+{
+   0x52, 0x09, 0x6A, 0xD5, 0x30, 0x36, 0xA5, 0x38, 0xBF, 0x40, 0xA3, 0x9E, 0x81, 0xF3, 0xD7, 0xFB,
+   0x7C, 0xE3, 0x39, 0x82, 0x9B, 0x2F, 0xFF, 0x87, 0x34, 0x8E, 0x43, 0x44, 0xC4, 0xDE, 0xE9, 0xCB,
+   0x54, 0x7B, 0x94, 0x32, 0xA6, 0xC2, 0x23, 0x3D, 0xEE, 0x4C, 0x95, 0x0B, 0x42, 0xFA, 0xC3, 0x4E,
+   0x08, 0x2E, 0xA1, 0x66, 0x28, 0xD9, 0x24, 0xB2, 0x76, 0x5B, 0xA2, 0x49, 0x6D, 0x8B, 0xD1, 0x25,
+   0x72, 0xF8, 0xF6, 0x64, 0x86, 0x68, 0x98, 0x16, 0xD4, 0xA4, 0x5C, 0xCC, 0x5D, 0x65, 0xB6, 0x92,
+   0x6C, 0x70, 0x48, 0x50, 0xFD, 0xED, 0xB9, 0xDA, 0x5E, 0x15, 0x46, 0x57, 0xA7, 0x8D, 0x9D, 0x84,
+   0x90, 0xD8, 0xAB, 0x00, 0x8C, 0xBC, 0xD3, 0x0A, 0xF7, 0xE4, 0x58, 0x05, 0xB8, 0xB3, 0x45, 0x06,
+   0xD0, 0x2C, 0x1E, 0x8F, 0xCA, 0x3F, 0x0F, 0x02, 0xC1, 0xAF, 0xBD, 0x03, 0x01, 0x13, 0x8A, 0x6B,
+   0x3A, 0x91, 0x11, 0x41, 0x4F, 0x67, 0xDC, 0xEA, 0x97, 0xF2, 0xCF, 0xCE, 0xF0, 0xB4, 0xE6, 0x73,
+   0x96, 0xAC, 0x74, 0x22, 0xE7, 0xAD, 0x35, 0x85, 0xE2, 0xF9, 0x37, 0xE8, 0x1C, 0x75, 0xDF, 0x6E,
+   0x47, 0xF1, 0x1A, 0x71, 0x1D, 0x29, 0xC5, 0x89, 0x6F, 0xB7, 0x62, 0x0E, 0xAA, 0x18, 0xBE, 0x1B,
+   0xFC, 0x56, 0x3E, 0x4B, 0xC6, 0xD2, 0x79, 0x20, 0x9A, 0xDB, 0xC0, 0xFE, 0x78, 0xCD, 0x5A, 0xF4,
+   0x1F, 0xDD, 0xA8, 0x33, 0x88, 0x07, 0xC7, 0x31, 0xB1, 0x12, 0x10, 0x59, 0x27, 0x80, 0xEC, 0x5F,
+   0x60, 0x51, 0x7F, 0xA9, 0x19, 0xB5, 0x4A, 0x0D, 0x2D, 0xE5, 0x7A, 0x9F, 0x93, 0xC9, 0x9C, 0xEF,
+   0xA0, 0xE0, 0x3B, 0x4D, 0xAE, 0x2A, 0xF5, 0xB0, 0xC8, 0xEB, 0xBB, 0x3C, 0x83, 0x53, 0x99, 0x61,
+   0x17, 0x2B, 0x04, 0x7E, 0xBA, 0x77, 0xD6, 0x26, 0xE1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0C, 0x7D
+};
+
 
 void Galaxy::Init()
 {
@@ -9125,6 +9195,84 @@ text_string Galaxy::StargateAddress2text(u64 address)
 	}
 	return tmp;
 }
+
+//convert galaxy coordinates to HyperSpace coordinates
+vector3d Galaxy::Galaxy2HyperSpace(vector2d galaxyCoordinates)
+{
+	//Algo:
+	//1) fractional part (sub lightyear) is remained
+	//2) inside square 16x16 ly coordinates are mixed by Rijndael s-box with additional seed-random variables
+	//3) seed-random variables is calculated by determined random generator (i.e. hash) from coordinates rounded to 16
+	f64 xInt, xFrac, yInt, yFrac;
+	s32 x, y;
+	s32 xHigh,yHigh;
+	u8 xLow, yLow, xy;
+	xInt = floor(galaxyCoordinates.X);
+	xFrac = galaxyCoordinates.X-xInt;
+	yInt = floor(galaxyCoordinates.Y);
+	yFrac = galaxyCoordinates.Y-yInt;
+	x = (s32)xInt;
+	y = (s32)yInt;
+	xLow = (u8)(x&15);
+	yLow = (u8)(y&15);
+	xHigh = x&0xFFFFFFF0;
+	yHigh = y&0xFFFFFFF0;
+	xy = xLow+(yLow<<4);
+
+	//mixing
+	galaxyRNG->SetSeedWithCoordinates(galaxySeed,16,xHigh,yHigh);
+	xy+=galaxyRNG->GetByteByOffset(0);
+	xy = sBox[xy];
+	xy+=galaxyRNG->GetByteByOffset(1);
+
+	xLow = xy&15;
+	yLow = xy>>4;
+	x = xHigh^xLow;
+	y = yHigh^yLow;
+	xInt = (f64)x;
+	yInt = (f64)y;
+
+	return vector3d((xInt + xFrac)*LIGHTYEAR/HYPERSPACE_SCALE,
+		(yInt + yFrac)*LIGHTYEAR/HYPERSPACE_SCALE,0);
+}
+
+//convert HyperSpace coordinates to galaxy coordinates
+vector2d Galaxy::HyperSpace2Galaxy(vector3d hyperSpaceCoordinates)
+{
+	f64 xFull, yFull, xInt, xFrac, yInt, yFrac;
+	s32 x, y;
+	s32 xHigh,yHigh;
+	u8 xLow, yLow, xy;
+	xFull = hyperSpaceCoordinates.X*HYPERSPACE_SCALE/LIGHTYEAR;
+	yFull = hyperSpaceCoordinates.Y*HYPERSPACE_SCALE/LIGHTYEAR;
+	xInt = floor(xFull);
+	xFrac = xFull-xInt;
+	yInt = floor(yFull);
+	yFrac = yFull-yInt;
+	x = (s32)xInt;
+	y = (s32)yInt;
+	xLow = (u8)(x&15);
+	yLow = (u8)(y&15);
+	xHigh = x&0xFFFFFFF0;
+	yHigh = y&0xFFFFFFF0;
+	xy = xLow+(yLow<<4);
+
+	//demixing
+	galaxyRNG->SetSeedWithCoordinates(galaxySeed,16,xHigh,yHigh);
+	xy-=galaxyRNG->GetByteByOffset(1);
+	xy = sBoxInv[xy];
+	xy-=galaxyRNG->GetByteByOffset(0);
+
+	xLow = xy&15;
+	yLow = xy>>4;
+	x = (x&0xFFFFFFF0)^xLow;
+	y = (y&0xFFFFFFF0)^yLow;
+	xInt = (f64)x;
+	yInt = (f64)y;
+
+	return vector2d(xInt + xFrac,yInt + yFrac);
+}
+
 
 //#########################################################################
 //GalaxyStarSystem
